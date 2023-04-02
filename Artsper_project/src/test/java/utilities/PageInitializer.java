c5833003ba5c3ac0be21410f2aefcedb4a65ae31
Kotchanop <kotchanopn@gmail.com>
2023-04-02 13:44:08 -0400

    Kotchanopfirstpush

--- Artsper_project/src/test/java/utilities/PageInitializer.java
+++ Artsper_project/src/test/java/utilities/PageInitializer.java
@@ -1,17 +1,24 @@
 package utilities;
 
 import pageObject.HomePage;
+import pageObject.HomePage2;
 import pageObject.LoginPage;
 
 public class PageInitializer extends Driver {
 
 	public static HomePage hp;
 	public static LoginPage lp;
+	
+	
+	public static HomePage2 hp2;
 
+	
 	public static void initialize() {
 		Driver.getDriver();
 		hp = new HomePage();
 		lp = new LoginPage();
+		
+		hp2 = new HomePage2();
 	}
 
 }
