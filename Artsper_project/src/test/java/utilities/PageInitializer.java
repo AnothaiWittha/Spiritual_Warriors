9d269c2e37c38a79ab8d47e2f1de3df19385cea1
AnothaiWittha <Anowtyptnk@gmail.com>
2023-04-05 16:44:09 -0400

    update

--- Artsper_project/src/test/java/utilities/PageInitializer.java
+++ Artsper_project/src/test/java/utilities/PageInitializer.java
@@ -5,11 +5,13 @@ import pageObject.HomePage;
 import pageObject.HomePage2;
 import pageObject.LoginPage;
 
+
 public class PageInitializer extends Driver {
 
 	public static HomePage hp;
 	public static LoginPage lp;
 	public static HomePage2 hp2;
+
 
 	
 	public static void initialize() {
@@ -18,7 +20,6 @@ public class PageInitializer extends Driver {
 		lp = new LoginPage();		
 		hp2 = new HomePage2();
 
-		
 	}
 
 }
