697e609f5513849e9a811db980280abd3f687698
Furtun741 <Furtun741@gmail.com>
2023-04-08 16:40:47 -0400

    Aysem 1th push

---
+++ Artsper_project/src/test/java/pageTest/BillingPageTestAysem.java
@@ -0,0 +1,40 @@
+package pageTest;
+
+import org.testng.annotations.AfterClass;
+
+import org.testng.annotations.BeforeMethod;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import utilities.Driver;
+import utilities.MyIListeners;
+import utilities.PageInitializer;
+
+@Listeners(MyIListeners.class)
+
+public class BillingPageTestAysem extends  PageInitializer{
+
+	@BeforeMethod
+	public void beforeTest() {
+		Driver.getDriver();
+	}
+	
+	@Test (priority = 1, enabled = true, groups = { "smokeTestSuit", "regressionSuite" })
+	public void searchBar() {
+		lp.loginSuccessfullyMethod();
+		bpa.searchBarMethod();
+	}
+	
+	
+	@Test (priority = 2, enabled = true, groups = { "smokeTestSuit", "regressionSuite" })
+	public void billingInformation() {
+		bpa.billingInfoMethod();
+	}
+
+	@AfterClass
+	public void afterClass() {
+		Driver.closeDriver();
+	}
+
+}
+
