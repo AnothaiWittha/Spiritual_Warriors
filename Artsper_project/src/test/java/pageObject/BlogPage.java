279f6122cbd83d9cb254f1a767653ba0c5e1a318
Rimer <rimertv@gmail.com>
2023-04-09 11:24:03 -0400

    yolo

--- Artsper_project/src/test/java/pageObject/BlogPage.java
+++ Artsper_project/src/test/java/pageObject/BlogPage.java
@@ -1,5 +1,7 @@
 package pageObject;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
@@ -18,24 +20,28 @@ public class BlogPage {
 	// Blog Page Link from Homepage
 	@FindBy(xpath = "//*[@id=\"app-container\"]/footer/div[2]/section/div[7]/ul/li[3]/a")
 	public WebElement BlogLink;
-	
-	//Best Art Movies in Blog page
+
+	// Best Art Movies in Blog page
 	@FindBy(xpath = "//*[@id=\"home-artstyle\"]/div[2]/div[4]/a/article/div/div[1]/div/img")
 	public WebElement TheBestArtMovies;
-	
+
+	@FindBy(xpath = "//*[@class=\"wp-block-heading\"]")
+	public List<WebElement> nameOfMovies;
+	// article[@class='article list carousel mb-0']
+
 	public void BlogClick() {
 		CommonMethods.scrollToElement(BlogLink);
 //		CommonMethods.ScrolByPixel(50000);
 		CommonMethods.wait(3);
 		BlogLink.click();
 	}
-	
+
 	public void ArtMoviesClick() {
 		CommonMethods.scrollToElement(TheBestArtMovies);
 //		CommonMethods.ScrolByPixel(50000);
 		CommonMethods.wait(3);
 		TheBestArtMovies.click();
+		CommonMethods.getElementsText(nameOfMovies);
 	}
-	
-	
+
 }
